f4f71de142872481b94eae39d3662f589758a51a
Mingbing-get <1508850533@qq.com>
2023-04-19 16:39:00 +0800

    区分button 的disabled和loading样式

--- packages/rabbit/preview/examples/button.tsx
+++ packages/rabbit/preview/examples/button.tsx
@@ -7,7 +7,7 @@ export default function ExampleButton() {
       <Button type='primary'>
         确认
       </Button>
-      <Button type='success' ghost>
+      <Button type='success'>
         确认
       </Button>
       <Button type='danger'>
@@ -16,10 +16,75 @@ export default function ExampleButton() {
       <Button type='warning'>
         确认
       </Button>
-      <Button disabled block>
+      <br />
+      <Button type='primary' ghost>
         确认
       </Button>
+      <Button type='success' ghost>
+        确认
+      </Button>
+      <Button type='danger' ghost>
+        确认
+      </Button>
+      <Button type='warning' ghost>
+        确认
+      </Button>
+      <br />
+      <Button disabled>
+        确认
+      </Button>
+      <Button type='primary' disabled>
+        确认
+      </Button>
+      <Button type='success' disabled>
+        确认
+      </Button>
+      <Button type='danger' disabled>
+        确认
+      </Button>
+      <Button type='warning' disabled>
+        确认
+      </Button>
+      <br />
+      <Button type='primary' ghost disabled>
+        确认
+      </Button>
+      <Button type='success' ghost disabled>
+        确认
+      </Button>
+      <Button type='danger' ghost disabled>
+        确认
+      </Button>
+      <Button type='warning' ghost disabled>
+        确认
+      </Button>
+      <br />
       <Button loading>
+        确认
+      </Button>
+      <Button type='primary' loading>
+        确认
+      </Button>
+      <Button type='success' loading>
+        确认
+      </Button>
+      <Button type='danger' loading>
+        确认
+      </Button>
+      <Button type='warning' loading>
+        确认
+      </Button>
+      <br />
+      <Button type='primary' ghost loading>
+        确认
+      </Button>
+      <Button type='success' ghost loading>
+        确认
+      </Button>
+      <Button type='danger' ghost loading>
+        确认
+      </Button>
+      <Button type='warning' ghost loading>
         确认
       </Button>
     </div>
