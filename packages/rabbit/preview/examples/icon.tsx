09942d54f15f71775f14e0dd68a0fe18336c42f9
Mingbing-get <1508850533@qq.com>
2023-10-17 17:09:20 +0800

    将svg图标写成字符串，准备兼容微信小程序

--- packages/rabbit/preview/examples/icon.tsx
+++ packages/rabbit/preview/examples/icon.tsx
@@ -4,14 +4,17 @@ import { Icon, iconPathKeys } from '../../../rabbit/src'
 export default function ExampleIcon() {
   return (
     <div style={{ display: 'grid', gridTemplateColumns: 'repeat(4, 23%)', justifyContent: 'space-between' }}>
-      {
-        iconPathKeys.map(key => (
-          <div key={key} style={{ padding: '1rem', borderRadius: '0.5rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-            <Icon type={key} style={{ fontSize: '2rem' }} />
-            <span style={{ marginTop: '0.4rem' }}>{key}</span>
-          </div>
-        ))
-      }
+      {iconPathKeys.map((key) => (
+        <div
+          key={key}
+          style={{ padding: '1rem', borderRadius: '0.5rem', display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+          <Icon
+            type={key}
+            style={{ fontSize: '2rem' }}
+          />
+          <span style={{ marginTop: '0.4rem' }}>{key}</span>
+        </div>
+      ))}
     </div>
   )
 }
